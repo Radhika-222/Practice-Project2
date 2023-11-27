dd4c3a8c98824bb9e91983f57bf2e221966d3015
Radhika-222 <wandareradhika@gmail.com>
2023-11-27 20:22:15 +0530

    Project

---
+++ Project/Project/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr;
+            try
+            {
+                string fpath = "E:\\Mphasis\\Project\\Student_data.txt";
+                Console.WriteLine("Student data:");
+                sr = new StreamReader(fpath);
+                string textfile = "";
+                while ((textfile = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(textfile);
+                }
+                sr.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error!!!" + ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
+        
+    
+
